83396bbd878e5bec127ab5a7c3cf4e739dd2c3aa
SofiiaPotapova <canaryfinch@gmail.com>
2021-04-21 11:46:21 +0300

    'new urls.py and templates directory +first html'

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class User(models.Model):
+#     id = models.AutoField()
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     city = models.CharField(max_length=20)
+#     email = models.EmailField()
+
--- core/views.py
+++ core/views.py
@@ -6,3 +6,5 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('ok')
+
+
