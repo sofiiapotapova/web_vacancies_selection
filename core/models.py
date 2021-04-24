9f2cd0ac06f151faf708eadeb7dd681b6dd08e21
SofiiaPotapova <canaryfinch@gmail.com>
2021-04-24 11:54:19 +0300

    'additional model User deleted'

--- core/models.py
+++ core/models.py
@@ -4,19 +4,6 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
-    first_name = models.CharField('Name', max_length=30)
-    last_name = models.CharField('Surname', max_length=30)
-    city = models.CharField('City', max_length=20)
-
-    def __str__(self):
-        return self.first_name
-
-    class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = 'Пользователи'
-
-
 class Competence(models.Model):
     ONE = '1'
     TWO = '2'
