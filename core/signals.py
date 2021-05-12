864f95059db811b11ebf10219956996f82dfb69a
SofiiaPotapova <canaryfinch@gmail.com>
2021-05-12 11:18:56 +0300

    'elasticsearch delete added'

--- core/signals.py
+++ core/signals.py
@@ -16,3 +16,15 @@ def update_document(sender, vacs):
             for _instance in instances:
                 registry.update(_instance)
 
+
+@receiver(post_delete)
+def delete_document(sender, vacs):
+    app_label = sender._meta.app_label
+    model_name = sender._meta.model_name
+    instance = vacs['instance']
+
+    if app_label == 'core':
+        if model_name == 'vacancy':
+            instances = instance.vacancies.all()
+            for _instance in instances:
+                registry.delete(_instance, raise_on_error=False)
